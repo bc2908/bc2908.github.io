7b62bcfcc4d7e23a8ee679727af6ecc545f16da7
bc2908 <sales9292@gmail.com>
2018-01-29 15:13:29 +0700

    new

--- CodeCamp1/codecamp.js
+++ CodeCamp1/codecamp.js
@@ -23,7 +23,8 @@ console.log(LuckyNum(123456));
 
 function FormatString(str) {
     var newStr = "";
-    var lastStr="";
+    var lastStr = "";
+    var n;
 
     for (var i = 0; i < str.length; i++) {
         if (str[i] != ",") {
@@ -31,13 +32,18 @@ function FormatString(str) {
         }
     }
 
-    newStr+=".";
+    newStr += ".";
 
     for (var j = 0; j < newStr.length; j++) {
-        if (j!=0) {
-            lastStr += newStr[j];
+        n = newStr.charCodeAt(j);
+        if (j != 0) {
+            if (n < 97 && n != 32 && n != 46) {
+                n += 32;
+                lastStr += String.fromCharCode(n);
+            } else {
+                lastStr += newStr[j];
+            }
         } else {
-            var n = newStr.charCodeAt(j);
             n -= 32;
             lastStr += String.fromCharCode(n);
         }
@@ -46,17 +52,17 @@ function FormatString(str) {
 }
 console.log(FormatString("welcome, to The Test"));
 
-function MaxArrList(arr){
+function MaxArrList(arr) {
     var max = arr[0];
-    for(var i=1;i<arr.length;i++){
-        if(max.length<arr[i].length){
+    for (var i = 1; i < arr.length; i++) {
+        if (max.length < arr[i].length) {
             max = arr[i];
         }
     }
-    
+
     var newArr = [];
-    for(var j=0;j<arr.length;j++){
-        if(arr[j].length == max.length){
+    for (var j = 0; j < arr.length; j++) {
+        if (arr[j].length == max.length) {
             newArr[newArr.length] = arr[j];
         }
     }
@@ -66,17 +72,17 @@ function MaxArrList(arr){
 arr = ["abc", "hello", "12345", "tech"];
 console.log(MaxArrList(arr));
 
-function SearchGold(weight1, value1, weight2, value2, weight3){
-    if(weight1+weight2>weight3){
-        if(value1>value2){
+function SearchGold(weight1, value1, weight2, value2, weight3) {
+    if (weight1 + weight2 > weight3) {
+        if (value1 > value2) {
             return value1;
-        }else{
+        } else {
             return value2;
         }
-    }else{
-        return value1+value2;
+    } else {
+        return value1 + value2;
     }
 }
-console.log(SearchGold(5,10,4,6,8));
-console.log(SearchGold(5,10,4,6,9));
+console.log(SearchGold(5, 10, 4, 6, 8));
+console.log(SearchGold(5, 10, 4, 6, 9));
 
